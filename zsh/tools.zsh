388508e16fe039132f556a9b910943a4510c86c5
hadisinaee <sinaee.hadi@gmail.com>
2026-05-06 14:31:49 -0700

    feat: add plugins, zoxide, history options, and keybindings

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -25,3 +25,32 @@ fi
 
 autoload -Uz compinit
 compinit
+
+# --- Keybindings ---
+bindkey -e
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
+
+# --- Completion styling ---
+zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+
+# --- Plugins (installed via scripts/install.sh) ---
+# fzf-tab must come after compinit and after fzf --zsh (so it can override Tab)
+[[ -f "$ZSH_CONFIG_ROOT/plugins/fzf-tab/fzf-tab.plugin.zsh" ]] && \
+  source "$ZSH_CONFIG_ROOT/plugins/fzf-tab/fzf-tab.plugin.zsh"
+
+[[ -f "$ZSH_CONFIG_ROOT/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" ]] && \
+  source "$ZSH_CONFIG_ROOT/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
+
+# zsh-syntax-highlighting must be sourced last
+[[ -f "$ZSH_CONFIG_ROOT/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]] && \
+  source "$ZSH_CONFIG_ROOT/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+
+# --- Zoxide ---
+# cd is replaced by zoxide; z is a shorthand alias
+if command -v zoxide >/dev/null 2>&1; then
+  eval "$(zoxide init --cmd cd zsh)"
+  alias z='cd'
+fi
